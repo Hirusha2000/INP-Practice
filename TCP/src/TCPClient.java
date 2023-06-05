6dcd72d78d5af1571542334e56a517b9b3ad964f
Hirusha2000 <2000hirushaudara@gmail.com>
2023-06-05 10:22:30 +0530

    TCP client process has baeen finished

--- TCP/src/TCPClient.java
+++ TCP/src/TCPClient.java
@@ -2,45 +2,34 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Locale;
 
 /**
  * @author: Hirusha Geeganage
- * created :6/5/2023--9:37 AM
+ * created :6/5/2023--9:36 AM
  **/
 
 public class TCPClient {
  public static void main(String[] args) throws IOException {
 
 
-  String clientSentence;
-  String capitalizedSentence;
+  String sentence;
+  String modifiedSentence;
 
-  System.out.println("Waiting for clients");
+  BufferedReader fromUser= new BufferedReader(new InputStreamReader(System.in));
 
-  ServerSocket serverSocket = new ServerSocket(9806);
+  Socket socket =new Socket("localhost",9806);
+  System.out.println("Enter a string");
+  DataOutputStream outtoServer =new DataOutputStream(socket.getOutputStream());
 
-  while (true){
-
-   Socket socket = serverSocket.accept();
-   System.out.println("Connection is established");
-   System.out.println("Getting client data");
+  BufferedReader infromserver= new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
 
-   // to read the string that sent by client
-   BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream())); // bufferader type object ekakata data da gatta
+  sentence=fromUser.readLine();
+  outtoServer.writeBytes(sentence +'\n');
 
-   DataOutputStream outToClient= new DataOutputStream(socket.getOutputStream());
-
-
-   clientSentence=fromClient.readLine();
-   capitalizedSentence=clientSentence.toUpperCase()+'\n';
-
-   outToClient.writeByte(Integer.parseInt(capitalizedSentence));
-  }
+  modifiedSentence=infromserver.readLine();
+  System.out.println(" From Server =" + modifiedSentence);
+  socket.close();
  }
-
-
 }
--- TCP/src/TCPServer.java
+++ TCP/src/TCPServer.java
@@ -1,10 +1,46 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Locale;
+
 /**
  * @author: Hirusha Geeganage
- * created :6/5/2023--9:36 AM
+ * created :6/5/2023--9:37 AM
  **/
 
 public class TCPServer {
- public static void main(String[] args) {
+ public static void main(String[] args) throws IOException {
 
+
+  String clientSentence;
+  String capitalizedSentence;
+
+  System.out.println("Waiting for clients");
+
+  ServerSocket serverSocket = new ServerSocket(9806);
+
+  while (true){
+
+   Socket socket = serverSocket.accept();
+   System.out.println("Connection is established");
+   System.out.println("Getting client data");
+
+
+   // to read the string that sent by client
+   BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream())); // bufferader type object ekakata data da gatta
+
+   DataOutputStream outToClient= new DataOutputStream(socket.getOutputStream());
+
+
+   clientSentence=fromClient.readLine();
+   capitalizedSentence=clientSentence.toUpperCase()+'\n';
+
+   outToClient.writeBytes(capitalizedSentence);
+  }
  }
+
+
 }
